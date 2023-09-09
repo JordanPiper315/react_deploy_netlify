3d19646ed1feff6342267c22f525be91265bd4e7
Jordan Piper <jordanannpiper@gmail.com>
2023-09-09 14:57:18 -0700

    Added usestate

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Content = () => {
-    const handleNameChange = () => {
-      const names = ['Jordan', 'Cahaela', 'Jessiah'];
-      const int = Math.floor(Math.random() * 3);
-      return names[int]
+  const [name, setName] = useState('Jordan');
+  const [count, setCount] = useState(0);
+  const handleNameChange = () => {
+    const names = ['Jordan', 'Cahaela', 'Jessiah'];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int])
   }
+
+  const handleClick = () => {
+    setCount(count+1)
+    console.log(count)
+  }
+  const handleClick2 = () => {
+    console.log(count)
+  }
+
   return (
     <main>
-      <p>
-        Hello {handleNameChange()}!
-      </p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={handleClick2}>Click it</button>
+      
     </main>
   )
 }
-
 export default Content
