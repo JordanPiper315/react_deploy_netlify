a73d4b8f80ab181a83065fb441f047e4fe733518
Jordan Piper <jordanannpiper@gmail.com>
2023-09-10 13:08:13 -0700

    added props and broke out list items into components

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,19 @@
 import React from 'react'
-import { useState } from 'react';
+import ItemList from './ItemList'
 
-const Content = () => {
-  const [name, setName] = useState('Jordan');
-  const [count, setCount] = useState(0);
-  const handleNameChange = () => {
-    const names = ['Jordan', 'Cahaela', 'Jessiah'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int])
-  }
-
-  const handleClick = () => {
-    setCount(count+1)
-    console.log(count)
-  }
-  const handleClick2 = () => {
-    console.log(count)
-  }
+const Content = ({ items, handleCheck, handleDelete }) => {
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}!</p>
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={handleClick2}>Click it</button>
-      
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}
     </main>
   )
 }
